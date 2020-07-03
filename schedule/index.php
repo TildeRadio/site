86166f3d3110c7b40e4654d9f6dd45a6f7d8009e
khuxkm fbexl <khuxkm@ttm.sh>
2020-07-03 02:51:01 -0400

    Add link to calendar on schedule page

--- schedule/index.php
+++ schedule/index.php
@@ -1,4 +1,5 @@
 <?php
+$additional_head='<link rel="alternate" type="text/calendar" href="https://tilderadio.org/schedule/ics.php">';
 include '../header.php';
 include 'schedule.php';
 ?>
@@ -6,6 +7,7 @@ include 'schedule.php';
 <h1><a href="https://tilderadio.org"><img style="width:75px;" src="../logos/tilderadio-green.png">tilderadio.org</a></h1>
 <h4>upcoming broadcasts</h4>
 <p>all times in UTC. current time is <span id="utcdate"><?=formatdate("now")?></span>.</p>
+<p>this schedule is also available in <a href="https://icalendar.org/validator.html?url=https://tilderadio.org/schedule/ics.php">iCalendar format</a>. point your calendar client at <code>https://tilderadio.org/schedule/ics.php</code>.</p>
 
 <table class="table table-striped">
     <thead>
