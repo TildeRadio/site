76abc458e74707235d09b44dd704558019480880
cmccabe <cmccabe@tilde.team>
2020-07-08 22:13:04 -0400

    relative link didn't work in schedule dir

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 </div>
 <footer class="text-center">
-    <a href="https://tildegit.org/tilderadio/site">page source</a> | <a href="dmca.php">DMCA</a>
+    <a href="https://tildegit.org/tilderadio/site">page source</a> | <a href="/dmca.php">DMCA</a>
 </footer>
 
 <script src="/js/jquery.min.js"></script>
