76c878d65a8efe1e5c4b23070e8b036f58603217
James Tomasino <james@tomasino.org>
2020-09-26 19:02:06 +0000

    allow for remote development of schedule by pointing to live schedule json in dev

--- schedule/schedule.php
+++ schedule/schedule.php
@@ -2,30 +2,36 @@
 include 'apikey.php';
 
 if (empty($apikey)) {
-    die("missing api key");
+  /* If we don't have the API key, assume we're developing. Pull data from live version of this file */
+  $context = stream_context_create([
+      "http" => [
+          "method" => "GET",
+      ]
+  ]);
+  $schedule = json_decode(file_get_contents("https://tilderadio.org/schedule/schedule.php?json=yes", false, $context), true);
+} else {
+  $context = stream_context_create([
+      "http" => [
+          "method" => "GET",
+          "header" => "X-API-Key: $apikey\r\n"
+      ]
+  ]);
+
+  // allow ics.php to overwrite $from and $to
+  if (!isset($from,$to)) {
+    $from = gmdate("Y-m-d\TH:i:s\Z", strtotime("now + 1 day"));
+    $to   = gmdate("Y-m-d\TH:i:s\Z", strtotime("now + 8 days"));
+  }
+
+  $schedule = json_decode(
+      file_get_contents(
+          "https://radio.tildeverse.org/api/station/1/streamers/schedule?start=$from&end=$to",
+          false,
+          $context
+      ),
+      true
+  );
 }
-
-$context = stream_context_create([
-    "http" => [
-        "method" => "GET",
-        "header" => "X-API-Key: $apikey\r\n"
-    ]
-]);
-
-// allow ics.php to overwrite $from and $to
-if (!isset($from,$to)) {
-	$from = gmdate("Y-m-d\TH:i:s\Z", strtotime("now + 1 day"));
-	$to   = gmdate("Y-m-d\TH:i:s\Z", strtotime("now + 8 days"));
-}
-
-$schedule = json_decode(
-    file_get_contents(
-        "https://radio.tildeverse.org/api/station/1/streamers/schedule?start=$from&end=$to",
-        false,
-        $context
-    ),
-    true
-);
 
 usort($schedule, function ($a, $b) {
     return $a["start"] <=> $b["start"];
