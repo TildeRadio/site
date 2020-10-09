a447477ed4538d1d6659d08b3a2ddda279b2db91
James Tomasino <james@tomasino.org>
2020-10-09 22:34:11 +0000

    adds manual stream info and restructures homepage a bit for readability and accessibility

--- index.php
+++ index.php
@@ -1,24 +1,30 @@
 <?php include 'header.php'; ?>
 
-<h1><a href="https://tilderadio.org"><img style="width:75px;" src="./logos/tilderadio-green.png">tilderadio.org</a></h1>
-	<h4>
-		<?=json_decode(file_get_contents("https://bot.tildegit.org/api/slogan"))?>
-	</h4>
+<h1>
+  <a href="/"><img style="width:75px;" src="./logos/tilderadio-green.png" alt="">tilderadio.org</a>
+</h1>
+<h4><?=json_decode(file_get_contents("https://bot.tildegit.org/api/slogan"))?></h4>
 
-<br>
-<br>
 <p>
-TildeRadio is Internet radio streamed by / for users of the tildeverse.
-<a href="https://tildeverse.org/">tildeverse.org</a>
+TildeRadio is Internet radio streamed by / for users of the <a href="https://tildeverse.org/">tildeverse</a>.
 </p>
-<p>TildeRadio has a dedicated irc channel, #tilderadio on <a href="https://tilde.chat">tilde.chat</a></p>
-<p>Join the conversation <a href="https://kiwi.tilde.chat/#tilderadio" target="_blank">Here</a></p>
-<br>
-<br>
-<p><?php include 'schedule/nextdj.php'; ?></p>
+
+<p><a href="https://kiwi.tilde.chat/#tilderadio" target="_blank">Join us in #tilderadio</a>, our dedicated IRC channel on <a href="https://tilde.chat">tilde.chat</a></p>
+
+
 <hr>
+<h4>how to listen</h4>
+<p><em><?php include 'schedule/nextdj.php'; ?></em></p>
 <p>
-<iframe src="https://radio.tildeverse.org/public/tilderadio/embed" frameborder="0" allowtransparency="true" style="width: 100%; min-height: 160px; border: 0;"></iframe>
+  <iframe src="https://radio.tildeverse.org/public/tilderadio/embed" frameborder="0" allowtransparency="true" style="width: 100%; min-height: 160px; border: 0;"></iframe>
+</p>
+
+<p>
+Or use the following links in your media player of choice:
+<ul>
+<li>https://radio.tildeverse.org/radio/8000/radio.ogg</li>
+<li>https://radio.tildeverse.org/radio/8000/radio.mp3</li>
+</ul>
 </p>
 
 <hr>
