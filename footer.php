b57596bd474ce245ccd39aaee2947e7dd800d0a2
Santiago Forero <santiago@forero.ga>
2021-09-01 12:39:26 -0500

    validate html

--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
-</div>
-<footer class="text-center">
-    <a href="https://tildegit.org/tilderadio/site">page source</a> | <a href="/dmca.php">DMCA</a>
-</footer>
+        <footer class="text-center">
+            <a href="https://tildegit.org/tilderadio/site">page source</a> | <a href="/dmca.php">DMCA</a>
+        </footer>
 
-<script src="/js/jquery.min.js"></script>
-<script src="/js/bootstrap.min.js"></script>
-</body>
+        <script src="/js/jquery.min.js"></script>
+        <script src="/js/bootstrap.min.js"></script>
+    </body>
 
 </html>
