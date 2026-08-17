f1ba6e6d20d1bc66b51f0978fac59b86e4759e16
deepend-tildeclub <58404188+deepend-tildeclub@users.noreply.github.com>
2026-08-17 13:09:30 -0600

    Update source link to GitHub in footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
             </main>
             <footer class="site-footer">
                 <span>tilderadio / internet radio from the tildeverse</span>
-                <span><a href="<?= htmlspecialchars(asset('stats/'), ENT_QUOTES, 'UTF-8') ?>">stats</a> <a href="https://tildegit.org/tilderadio/site">source</a> <a href="<?= htmlspecialchars(asset('dmca.php'), ENT_QUOTES, 'UTF-8') ?>">dmca</a></span>
+                <span><a href="<?= htmlspecialchars(asset('stats/'), ENT_QUOTES, 'UTF-8') ?>">stats</a> <a href="https://github.com/TildeRadio/site">source</a> <a href="<?= htmlspecialchars(asset('dmca.php'), ENT_QUOTES, 'UTF-8') ?>">dmca</a></span>
             </footer>
         </div>
     </body>
